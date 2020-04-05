7749a45447adc58581542132e77de4bbcc7ac357
Adebayo Rilwan Ajibola <a.adebayo@smefunds.com>
2020-04-05 12:08:35 +0100

    added build folder manually

---
+++ build/precache-manifest.930b8dc33f5140dc7df9327e04f8b8f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee297bd056fece6b70f9a2387060aa3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06d7507eb327d40b59e7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "231b02e729404b00071b",
+    "url": "/static/js/2.1350b6de.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/static/js/2.1350b6de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d7507eb327d40b59e7",
+    "url": "/static/js/main.0798d676.chunk.js"
+  },
+  {
+    "revision": "d21afc6f16192b66d7af",
+    "url": "/static/js/runtime-main.56c2b337.js"
+  }
+]);
